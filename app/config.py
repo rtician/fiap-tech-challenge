ea68d8feabfdbcec1a56a56a841f893b7f05ffdd
Ramiro Tician <ramirotician@proton.me>
2024-10-16 19:17:08 -0300

    Initial setup (#1)

---
+++ app/config.py
@@ -0,0 +1,9 @@
+DATABASE_URL = ""
+
+CONFIG_DB_HOST: str = "localhost"
+CONFIG_DB_NAME: str = "app_db"
+CONFIG_DB_USER: str = "user"
+CONFIG_DB_PASSWORD: str = "randomP*ssword!"
+SQLALCHEMY_DATABASE_URL: str = (
+    f"postgresql://{CONFIG_DB_USER}:{CONFIG_DB_PASSWORD}@{CONFIG_DB_HOST}/{CONFIG_DB_NAME}"
+)
---
+++ app/domain/entities/customer.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Customer(BaseModel):
+    id: Optional[int] = None
+    name: str
+    cpf: str
+    email: Optional[str] = None
