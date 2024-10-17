4b2f1e6a6c4350fede9110ac22be604a174deb3c
Ramiro Tician <ramirotician@proton.me>
2024-10-17 13:28:58 -0300

    Fixed app startup (#2)

--- app/adapters/api/customer.py
+++ app/adapters/api/customer.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends
+
+from app.adapters.db.customer_repository import SQLCustomerRepository
 from app.domain.entities.customer import Customer
 from app.domain.services.customer_service import CustomerService
 
@@ -6,5 +8,5 @@ router = APIRouter()
 
 
 @router.post("/customers", response_model=Customer)
-def register_customer(customer: Customer, service: CustomerService = Depends()):
+def register_customer(customer: Customer, service: CustomerService = Depends(SQLCustomerRepository)):
     return service.register_customer(customer)
