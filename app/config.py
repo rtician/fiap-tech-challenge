b7eb9dfb5cd80158c763dd5da2a935c2d7abd219
Ramiro Tician <ramirotician@proton.me>
2024-10-21 11:33:10 -0300

    GitHub CI pre-commit validation (#6)
    * GitHub CI pre-commit validation
    
    * Fixed lint

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ CONFIG_DB_NAME: str = "app_db"
 CONFIG_DB_USER: str = "user"
 CONFIG_DB_PASSWORD: str = "randomP*ssword!"
 SQLALCHEMY_DATABASE_URL: str = (
-    f"postgresql://{CONFIG_DB_USER}:{CONFIG_DB_PASSWORD}@{CONFIG_DB_HOST}/{CONFIG_DB_NAME}"
+    f"postgresql://{CONFIG_DB_USER}:{CONFIG_DB_PASSWORD}@{CONFIG_DB_HOST}/{CONFIG_DB_NAME}"  # noqa
 )
