f967ea4ecc5717c571410944aefd40703b2810fb
Ramiro Tician <ramirotician@proton.me>
2024-10-27 13:39:48 -0300

    Endpoint to get customer by cpf

--- app/adapters/api/customer.py
+++ app/adapters/api/customer.py
@@ -8,10 +8,10 @@ from app.domain.services.customer_service import CustomerService
 from app.domain.services.customer_service import get_customer_service
 from app.domain.services.exceptions import CpfAlreadyExists
 
-router = APIRouter()
+router = APIRouter(prefix="/customers")
 
 
-@router.post("/customers", response_model=Customer)
+@router.post("", response_model=Customer)
 def register_customer(
     customer_request: CustomerRequest, service: CustomerService = Depends(get_customer_service)
 ):
@@ -20,3 +20,11 @@ def register_customer(
         return service.register_customer(customer)
     except CpfAlreadyExists as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("", response_model=Customer)
+def get_customer_by_cpf(cpf: str, service: CustomerService = Depends(get_customer_service)):
+    customer = service.get_customer(cpf)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return customer
--- app/domain/services/customer_service.py
+++ app/domain/services/customer_service.py
@@ -3,6 +3,7 @@ from app.adapters.repository.customer_repository import SQLCustomerRepository
 from app.domain.entities.customer import Customer
 from app.domain.repositories.customer_repository import ICustomerRepository
 from app.domain.services.exceptions import CpfAlreadyExists
+from app.domain.services.exceptions import NotFound
 
 
 class CustomerService:
@@ -14,6 +15,12 @@ class CustomerService:
         if existing_customer:
             raise CpfAlreadyExists("Customer with this CPF already exists.")
         return self.customer_repository.add_customer(customer)
+
+    def get_customer(self, cpf: str) -> Customer:
+        customer = self.customer_repository.get_customer_by_cpf(cpf)
+        if not customer:
+            raise NotFound("Customer with this CPF not found.")
+        return customer
 
 
 def get_customer_service() -> CustomerService:
